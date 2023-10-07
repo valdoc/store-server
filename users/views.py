8959e1468822611ec1be1ba7a64173c01458155e
valdoc <valdoc@yandex.ru>
2023-10-07 06:28:28 +0300

    add form users

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from users.forms import UserLoginForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    context = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', context=context)
 
 
 def registration(request):
