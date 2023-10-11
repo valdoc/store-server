fbfaac0a34979c0bf9fa883a7b0bd634ff53cd5a
valdoc <valdoc@yandex.ru>
2023-10-11 05:20:27 +0300

    UserLoginView, LogoutView

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from products.views import IndexView
 from django.conf.urls.static import static
 from django.conf import settings
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- users/views.py
+++ users/views.py
@@ -1,30 +1,35 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from django.contrib import auth, messages
 
 from products.models import Basket
 from users.models import User
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
 
-    else:
-        form = UserLoginForm()
-    context = {'form': form}
-    return render(request, 'users/login.html', context=context)
+
+#
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username=username, password=password)
+#             if user:
+#                 auth.login(request, user)
+#                 return HttpResponseRedirect(reverse('index'))
+#
+#     else:
+#         form = UserLoginForm()
+#     context = {'form': form}
+#     return render(request, 'users/login.html', context=context)
 
 
 class UserRegistrationView(CreateView):
@@ -38,18 +43,6 @@ class UserRegistrationView(CreateView):
         context['title'] = 'Store - Регистрация'
         return context
 
-
-# def registration(request):
-#     if request.method == 'POST':
-#         form = UserRegistrationForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Вы успешно зарегистрировались!')
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else:
-#         form = UserRegistrationForm()
-#     context = {'form': form}
-#     return render(request, 'users/register.html', context=context)
 
 class UserProfileView(UpdateView):
     model = User
@@ -65,29 +58,7 @@ class UserProfileView(UpdateView):
         context['baskets'] = Basket.objects.filter(user=self.request.user)
         return context
 
-
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('users:profile'))
-#     else:
-#         form = UserProfileForm(instance=request.user)
 #
-#     baskets = Basket.objects.filter(user=request.user)
-#
-#     context = {
-#         'title': 'Store - Профиль',
-#         'form': form,
-#         'baskets': baskets,
-#         'total_sum': sum([basket.sum() for basket in baskets]),
-#         'total_quantity': sum([basket.quantity for basket in baskets])
-#     }
-#     return render(request, 'users/profile.html', context=context)
-
-
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
+# def logout(request):
+#     auth.logout(request)
+#     return HttpResponseRedirect(reverse('index'))
